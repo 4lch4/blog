5413838af6aa891acc32c4740472d33b261df700
4lch4 <git@4lch4.dev>
2021-07-04 10:07:18 -0500

    feat: updated projects data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,18 @@
 const projectsData = [
   {
-    title: 'Knowledge Base',
-    description: `I created a website to serve as a collection of my "shared
-    knowledge". At the time of writing, this includes TIL snippets, cheatsheets,
-    interesting articles I found online, etc.`,
+    title: 'Digital Garden',
+    description: `A website to serve as my "digital garden" where I cultivate knowledge and
+    experience in public. Less pretentiously, it's where I post things like TIL snippets,
+    cheatsheets, and interesting articles I find online.`,
     imgSrc: '/static/images/kba-logo.svg',
-    href: 'https://www.google.com'
+    href: 'https://garden.4lch4.dev'
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten handphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine'
+    title: 'My Socials',
+    description: `A really small website that serves a collection of links to my various social
+    media profiles.`,
+    imgSrc: '/static/images/socials-0.svg',
+    href: 'https://4lch4.social'
   }
 ]
 
